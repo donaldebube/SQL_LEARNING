9dcce917fe7e302a7991e3efb45c590a3148f569
Donald Ebube <donaldebube@yahoo.com>
2022-10-17 23:34:11 +0100

    New Commit

--- SQL NEW TUTORIAL CODE.sql
+++ SQL NEW TUTORIAL CODE.sql
@@ -1762,3 +1762,5 @@ GO
 
 -- 1.) After Triggers, fires after the triggering action. The INSERT, UPDATE and DELETE statements, causes an after trigger to fire the respective statements complete execution.
 -- 2.) INSTEAD OF Triggers, fires instead of the triggering action. The INSERT, UPDATE and DELETE statements causes an INSTEAD OF Trigger to fire INSTEAD OF the respective statement execution
+
+ 
