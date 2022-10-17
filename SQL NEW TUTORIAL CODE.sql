dc674051698748726793cc79cbcdce24ed3cadcf
Donald Ebube <donaldebube@yahoo.com>
2022-10-17 23:20:01 +0100

    COMMIT

--- SQL NEW TUTORIAL CODE.sql
+++ SQL NEW TUTORIAL CODE.sql
@@ -1758,3 +1758,7 @@ GO
 
 -- DML Triggers can be gain classified into 2 types.
 -- 1.) After triggers (Sometimes called as FOR triggers)
+-- 2.) Instead of triggers
+
+-- 1.) After Triggers, fires after the triggering action. The INSERT, UPDATE and DELETE statements, causes an after trigger to fire the respective statements complete execution.
+-- 2.) INSTEAD OF Triggers, fires instead of the triggering action. The INSERT, UPDATE and DELETE statements causes an INSTEAD OF Trigger to fire INSTEAD OF the respective statement execution
