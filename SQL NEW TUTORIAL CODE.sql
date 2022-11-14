e448325a2f1d327b460364d40aeec23041347d29
Donald Ebube <donaldebube@yahoo.com>
2022-11-14 22:52:27 +0100

    NEW COMMIT

--- SQL NEW TUTORIAL CODE.sql
+++ SQL NEW TUTORIAL CODE.sql
@@ -2261,9 +2261,9 @@ AS
     ON SI.[Manager ID] = EC.[Employee ID]
 )
 SELECT 
-    Emp.[Full Name] AS [Employee Name], 
-    ISNULL(Mgr.[Full Name], 'Super Boss') AS [Manager Name],
-    Emp.[Level]
+    -- Emp.[Full Name] AS [Employee Name], 
+    -- ISNULL(Mgr.[Full Name], 'Super Boss') AS [Manager Name],
+    -- Emp.[Level]
 FROM EmployeesCTE AS Emp
 LEFT JOIN EmployeesCTE AS Mgr
 ON Emp.[Manager ID] = Mgr.[Employee ID]
