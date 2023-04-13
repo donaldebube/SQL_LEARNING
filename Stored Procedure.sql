0b72be92caf1976cab6145898269c3a7b9cbeac0
Donald Ebube <donaldebube@yahoo.com>
2023-04-13 09:45:11 +0100

    New Commit

--- Stored Procedure.sql
+++ Stored Procedure.sql
@@ -1,4 +1,18 @@
--- Create table to practice with
+-- A stored procedure is a group of T-SQL (Transact SQL)statements.
+-- Example 1:
+
+-- Get tblEmployee table from main query
+SELECT * 
+INTO #temp_tblEmployee
+FROM tblEmployees1
+
+SELECT *
+FROM #temp_tblEmployee
+
+-- Add some more rows to the dataset
+
+
+
 SELECT *
 INTO #temp_tblPerson
 FROM tblPerson
