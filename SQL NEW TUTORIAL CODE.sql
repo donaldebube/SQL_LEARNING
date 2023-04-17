a660b6cd681cc63ebba25c6a125aae7b6d6f7589
Donald Ebube <donaldebube@yahoo.com>
2023-04-17 16:38:42 +0100

    New Commit

--- SQL NEW TUTORIAL CODE.sql
+++ SQL NEW TUTORIAL CODE.sql
@@ -1611,8 +1611,8 @@ GO
 
 -- Part 41 - Indexed Views
 -- What happens when you create an index on a view?
--- A standard or non-standard view us just a stored SQL query. When we try to retrieve data from the view, the data is actually retrieved from the underlying base tables.
--- A VIEW id just a virtual table that does not store any data, by default.
+-- A standard or non-standard view is just a stored SQL query. When we try to retrieve data from the view, the data is actually retrieved from the underlying base tables.
+-- A VIEW id is just a virtual table that does not store any data, by default.
 -- However, when you create an index pn a VIEW, the view gets materialized. This means, the view is now capable of storing data.
 -- In SQL server, we call them Indexed views and in Oracle, Materialized Views
 
